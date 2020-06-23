3f1ef5144750a9921e62d2218821eca2450a1262
zhongqf <zhongqf@gmail.com>
2020-06-23 07:00:59 +0000

    initial commit

---
+++ src/components/Counter.tsx
@@ -0,0 +1,15 @@
+import { defineComponent, reactive } from 'vue';
+
+const Counter = defineComponent({
+    setup() {
+        const state = reactive({ count: 0 });
+        const handleClick = () => state.count++;
+        return () => (
+            <button onClick={handleClick}>
+                count is: {state.count}
+            </button>
+        )
+    }
+});
+
+export default Counter;
